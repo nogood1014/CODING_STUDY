2b5eb9a7b9254aa7b2f90fa7873f7296c948cc1a
Nohjiwon <65989563+nogood1014@users.noreply.github.com>
2020-07-08 16:39:46 +0900

    Create N2.c

---
+++ 첫번째 과제/N2.c
@@ -0,0 +1,16 @@
+//[ 기찍 N ] - C언어
+#include <stdio.h>
+
+int main(void)
+{
+    int i,N;
+    
+    scanf("%d",&N);
+    
+    for(i=N; i>0; i--)
+    {
+        printf("%d\n",i);
+    }
+    
+    return 0;
+}
