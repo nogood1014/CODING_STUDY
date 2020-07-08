250664f2e6b70f59ac23ae83d552cadb23624441
Nohjiwon <65989563+nogood1014@users.noreply.github.com>
2020-07-08 16:41:00 +0900

    Create star1.java

---
+++ 첫번째 과제/star1.java
@@ -0,0 +1,23 @@
+// [ 별 찍기 - 1 ]
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ 
+		Scanner s = new Scanner(System.in);
+		
+		int N = s.nextInt();
+		
+		for(int i=1; i<=N; i++) {
+			for(int j =1; j<=i; j++) {
+				System.out.print("*");
+			}
+			System.out.println("");
+		}
+		
+		
+	}
+
+}
