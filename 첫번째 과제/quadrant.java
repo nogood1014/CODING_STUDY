96dac8d5aec6a9dc362e41ee2dc489e12ef79912
Nohjiwon <65989563+nogood1014@users.noreply.github.com>
2020-07-08 16:34:39 +0900

    Create quadrant.java

---
+++ 첫번째 과제/quadrant.java
@@ -0,0 +1,28 @@
+//[ 사분면 고르기 ]
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ 
+		Scanner s = new Scanner(System.in);
+		
+		int x = s.nextInt();
+		int y = s.nextInt();
+		
+		if(x>0 && y>0) {
+			System.out.println("1");
+		}else if(x<0 && y>0) {
+			System.out.println("2");
+		}else if(x<0 && y<0) {
+			System.out.println("3");
+		}else if(x>0 && y<0) {
+			System.out.println("4");
+		}
+	
+	
+		
+	}
+
+}
