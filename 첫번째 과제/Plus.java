992655d0dc87402bdc8306d4c42914687cdb4437
Nohjiwon <65989563+nogood1014@users.noreply.github.com>
2020-07-08 16:37:28 +0900

    Create Plus.java

---
+++ 첫번째 과제/Plus.java
@@ -0,0 +1,20 @@
+//[ 합 ]
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ 
+		Scanner s = new Scanner(System.in);
+		
+		int n = s.nextInt();
+		int sum =0;
+		
+		for(int i = 1; i<=n; i++) {
+			sum =sum+i;
+		}
+		System.out.println(sum);
+
+	}
+}
