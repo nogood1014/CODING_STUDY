a20e95d025c992e5675b44702e08c67f637ad141
Nohjiwon <65989563+nogood1014@users.noreply.github.com>
2020-07-08 16:36:57 +0900

    Create A+B-3.java

---
+++ 첫번째 과제/A+B-3.java
@@ -0,0 +1,27 @@
+// [ A+B-3 ]
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ 
+		Scanner s = new Scanner(System.in);
+		
+		int T = s.nextInt();
+		int num1 =0;
+		int num2 =0;
+		
+		for(int i =0; i<T; i++) {
+			 num1 = s.nextInt();
+			 num2 = s.nextInt();
+			 
+			 System.out.println(num1+num2);
+		}
+		
+		
+		
+		
+	}
+
+}
