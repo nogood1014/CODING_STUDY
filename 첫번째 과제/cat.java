5f8d020e0c83ad7055ed97c596c634b8890330f6
Nohjiwon <65989563+nogood1014@users.noreply.github.com>
2020-07-08 16:25:52 +0900

    Create cat.java

---
+++ 첫번째 과제/cat.java
@@ -0,0 +1,13 @@
+//[ 고양이 ]
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.print(" \\(__)|");
+		
+		
+	}
+
