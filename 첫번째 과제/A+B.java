799b05f4135059ed576ca63a79cb2652c20d8684
Nohjiwon <65989563+nogood1014@users.noreply.github.com>
2020-07-08 16:26:51 +0900

    Create A+B.java

---
+++ 첫번째 과제/A+B.java
@@ -0,0 +1,23 @@
+//[ A+B ]
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ 
+		Scanner s = new Scanner(System.in);
+		
+		int A = s.nextInt();
+		int B = s.nextInt();
+		
+		if(A<=0 || B>=10) {
+			return;
+		}
+		System.out.println(A+B);
+
+				
+		
+	}
+
+}
